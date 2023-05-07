a41bea0e3f8b21357ea6b7024913e1768c8ba54d
opencoresam <132782902+opencoresam@users.noreply.github.com>
2023-05-07 09:49:46 -0700

    serve client in docker image (#6)

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
-FROM golang:1.20-alpine as builder
+# build the backend
+FROM golang:1.20-alpine as backend
 
 WORKDIR /code
 
-COPY go.mod /code/
-COPY go.sum /code/
+COPY go.mod go.sum ./
 
 RUN go mod download all
 
@@ -12,14 +12,39 @@ COPY . /code
 RUN mkdir bin
 RUN go build -o ./bin/server ./cmd/server 
 
-FROM alpine:latest
+# build the client deps
+FROM node:16-alpine as clientdeps
+RUN apk add --no-cache libc6-compat nasm autoconf automake bash libltdl libtool gcc make g++ zlib-dev
+WORKDIR /code
+COPY client/package.json client/yarn.lock ./
+RUN yarn install
+
+FROM node:16-alpine as client
+ENV NEXT_TELEMETRY_DISABLED 1
+WORKDIR /code
+COPY --from=clientdeps /code/node_modules ./node_modules
+COPY ./client .
+RUN yarn build
+
+# final request serving image
+FROM node:16-alpine
 
 RUN apk --no-cache add ca-certificates
 
 WORKDIR /root/
 
-COPY --from=builder /code/bin/server ./
-COPY dist /root/dist
-COPY gen /root/gen
+# copy backend resources
+COPY --from=backend /code/bin/server ./
+COPY dist ./dist
+COPY gen ./gen
+
+# copy client resources
+RUN mkdir /root/client
+COPY --from=client /code/public ./client/public
+COPY --from=client /code/package.json ./client/package.json
+COPY --from=client /code/.next ./client/.next
+COPY --from=client /code/node_modules ./client/node_modules
+
+ENV NEXT_TELEMETRY_DISABLED 1
 
 CMD ["./server"]
--- gen/protos/go/protos/fira/v1/api.pb.gw.go
+++ gen/protos/go/protos/fira/v1/api.pb.gw.go
@@ -63,7 +63,7 @@ func RegisterFiraServiceHandlerServer(ctx context.Context, mux *runtime.ServeMux
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
 		var err error
 		var annotatedContext context.Context
-		annotatedContext, err = runtime.AnnotateIncomingContext(ctx, mux, req, "/protos.fira.v1.FiraService/GetApiInfo", runtime.WithHTTPPathPattern("/info"))
+		annotatedContext, err = runtime.AnnotateIncomingContext(ctx, mux, req, "/protos.fira.v1.FiraService/GetApiInfo", runtime.WithHTTPPathPattern("/api/v1/info"))
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
@@ -127,7 +127,7 @@ func RegisterFiraServiceHandlerClient(ctx context.Context, mux *runtime.ServeMux
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
 		var err error
 		var annotatedContext context.Context
-		annotatedContext, err = runtime.AnnotateContext(ctx, mux, req, "/protos.fira.v1.FiraService/GetApiInfo", runtime.WithHTTPPathPattern("/info"))
+		annotatedContext, err = runtime.AnnotateContext(ctx, mux, req, "/protos.fira.v1.FiraService/GetApiInfo", runtime.WithHTTPPathPattern("/api/v1/info"))
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
@@ -147,7 +147,7 @@ func RegisterFiraServiceHandlerClient(ctx context.Context, mux *runtime.ServeMux
 }
 
 var (
-	pattern_FiraService_GetApiInfo_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0}, []string{"info"}, ""))
+	pattern_FiraService_GetApiInfo_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"api", "v1", "info"}, ""))
 )
 
 var (
