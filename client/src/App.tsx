5dc0ed6aef18dfe84c79bab5ce3d18ed36f3d02c
harry.hexhash <129999410+harryhexhash@users.noreply.github.com>
2023-05-08 00:21:25 -0600

    add rasa font (#9)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,12 @@
 import { AppProviders } from 'src/AppProviders';
 import { Layout } from 'src/components/layout/Layout';
 
+import { GlobalStyle } from './theme/GlobalStyle';
+
 export function App({ Component, pageProps }) {
   return (
     <AppProviders>
+      <GlobalStyle />
       <Layout>
         <Component {...pageProps} />
       </Layout>
---
+++ client/src/components/WordMark.tsx
@@ -0,0 +1,5 @@
+import { Heading } from '@chakra-ui/react';
+
+export function WordMark() {
+  return <Heading fontFamily="rasa">fira</Heading>;
+}
