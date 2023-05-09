ced5482a2d0c1f23b7bd741ee4f7aff2372fdc50
pnwx <pnwx@opencorelabs.org>
2023-05-08 20:15:49 -0700

    add link API defs

--- gen/protos/go/protos/fira/v1/api.pb.gw.go
+++ gen/protos/go/protos/fira/v1/api.pb.gw.go
@@ -49,6 +49,92 @@ func local_request_FiraService_GetApiInfo_0(ctx context.Context, marshaler runti
 
 }
 
+func request_FiraService_CreateLinkSession_0(ctx context.Context, marshaler runtime.Marshaler, client FiraServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq CreateLinkSessionRequest
+	var metadata runtime.ServerMetadata
+
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.CreateLinkSession(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_FiraService_CreateLinkSession_0(ctx context.Context, marshaler runtime.Marshaler, server FiraServiceServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq CreateLinkSessionRequest
+	var metadata runtime.ServerMetadata
+
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := server.CreateLinkSession(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
+func request_FiraService_GetLinkSession_0(ctx context.Context, marshaler runtime.Marshaler, client FiraServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq GetLinkSessionRequest
+	var metadata runtime.ServerMetadata
+
+	var (
+		val string
+		ok  bool
+		err error
+		_   = err
+	)
+
+	val, ok = pathParams["link_id"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "link_id")
+	}
+
+	protoReq.LinkId, err = runtime.String(val)
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "link_id", err)
+	}
+
+	msg, err := client.GetLinkSession(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_FiraService_GetLinkSession_0(ctx context.Context, marshaler runtime.Marshaler, server FiraServiceServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq GetLinkSessionRequest
+	var metadata runtime.ServerMetadata
+
+	var (
+		val string
+		ok  bool
+		err error
+		_   = err
+	)
+
+	val, ok = pathParams["link_id"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "link_id")
+	}
+
+	protoReq.LinkId, err = runtime.String(val)
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "link_id", err)
+	}
+
+	msg, err := server.GetLinkSession(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
 // RegisterFiraServiceHandlerServer registers the http handlers for service FiraService to "mux".
 // UnaryRPC     :call FiraServiceServer directly.
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
@@ -77,6 +163,56 @@ func RegisterFiraServiceHandlerServer(ctx context.Context, mux *runtime.ServeMux
 		}
 
 		forward_FiraService_GetApiInfo_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
+	mux.Handle("POST", pattern_FiraService_CreateLinkSession_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		var stream runtime.ServerTransportStream
+		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		var err error
+		var annotatedContext context.Context
+		annotatedContext, err = runtime.AnnotateIncomingContext(ctx, mux, req, "/protos.fira.v1.FiraService/CreateLinkSession", runtime.WithHTTPPathPattern("/api/v1/link"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_FiraService_CreateLinkSession_0(annotatedContext, inboundMarshaler, server, req, pathParams)
+		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
+		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
+		if err != nil {
+			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_FiraService_CreateLinkSession_0(annotatedContext, mux, outboundMarshaler, w, req, response_FiraService_CreateLinkSession_0{resp}, mux.GetForwardResponseOptions()...)
+
+	})
+
+	mux.Handle("GET", pattern_FiraService_GetLinkSession_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		var stream runtime.ServerTransportStream
+		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		var err error
+		var annotatedContext context.Context
+		annotatedContext, err = runtime.AnnotateIncomingContext(ctx, mux, req, "/protos.fira.v1.FiraService/GetLinkSession", runtime.WithHTTPPathPattern("/api/v1/link/{link_id}"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_FiraService_GetLinkSession_0(annotatedContext, inboundMarshaler, server, req, pathParams)
+		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
+		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
+		if err != nil {
+			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_FiraService_GetLinkSession_0(annotatedContext, mux, outboundMarshaler, w, req, response_FiraService_GetLinkSession_0{resp}, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -143,13 +279,83 @@ func RegisterFiraServiceHandlerClient(ctx context.Context, mux *runtime.ServeMux
 
 	})
 
+	mux.Handle("POST", pattern_FiraService_CreateLinkSession_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		var err error
+		var annotatedContext context.Context
+		annotatedContext, err = runtime.AnnotateContext(ctx, mux, req, "/protos.fira.v1.FiraService/CreateLinkSession", runtime.WithHTTPPathPattern("/api/v1/link"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_FiraService_CreateLinkSession_0(annotatedContext, inboundMarshaler, client, req, pathParams)
+		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
+		if err != nil {
+			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_FiraService_CreateLinkSession_0(annotatedContext, mux, outboundMarshaler, w, req, response_FiraService_CreateLinkSession_0{resp}, mux.GetForwardResponseOptions()...)
+
+	})
+
+	mux.Handle("GET", pattern_FiraService_GetLinkSession_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		var err error
+		var annotatedContext context.Context
+		annotatedContext, err = runtime.AnnotateContext(ctx, mux, req, "/protos.fira.v1.FiraService/GetLinkSession", runtime.WithHTTPPathPattern("/api/v1/link/{link_id}"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_FiraService_GetLinkSession_0(annotatedContext, inboundMarshaler, client, req, pathParams)
+		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
+		if err != nil {
+			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_FiraService_GetLinkSession_0(annotatedContext, mux, outboundMarshaler, w, req, response_FiraService_GetLinkSession_0{resp}, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
+}
+
+type response_FiraService_CreateLinkSession_0 struct {
+	proto.Message
+}
+
+func (m response_FiraService_CreateLinkSession_0) XXX_ResponseBody() interface{} {
+	response := m.Message.(*CreateLinkSessionResponse)
+	return response.LinkSession
+}
+
+type response_FiraService_GetLinkSession_0 struct {
+	proto.Message
+}
+
+func (m response_FiraService_GetLinkSession_0) XXX_ResponseBody() interface{} {
+	response := m.Message.(*GetLinkSessionResponse)
+	return response.LinkSession
 }
 
 var (
 	pattern_FiraService_GetApiInfo_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"api", "v1", "info"}, ""))
+
+	pattern_FiraService_CreateLinkSession_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"api", "v1", "link"}, ""))
+
+	pattern_FiraService_GetLinkSession_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3}, []string{"api", "v1", "link", "link_id"}, ""))
 )
 
 var (
 	forward_FiraService_GetApiInfo_0 = runtime.ForwardResponseMessage
+
+	forward_FiraService_CreateLinkSession_0 = runtime.ForwardResponseMessage
+
+	forward_FiraService_GetLinkSession_0 = runtime.ForwardResponseMessage
 )
--- gen/protos/go/protos/fira/v1/api_grpc.pb.go
+++ gen/protos/go/protos/fira/v1/api_grpc.pb.go
@@ -19,7 +19,9 @@ import (
 const _ = grpc.SupportPackageIsVersion7
 
 const (
-	FiraService_GetApiInfo_FullMethodName = "/protos.fira.v1.FiraService/GetApiInfo"
+	FiraService_GetApiInfo_FullMethodName        = "/protos.fira.v1.FiraService/GetApiInfo"
+	FiraService_CreateLinkSession_FullMethodName = "/protos.fira.v1.FiraService/CreateLinkSession"
+	FiraService_GetLinkSession_FullMethodName    = "/protos.fira.v1.FiraService/GetLinkSession"
 )
 
 // FiraServiceClient is the client API for FiraService service.
@@ -27,6 +29,11 @@ const (
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type FiraServiceClient interface {
 	GetApiInfo(ctx context.Context, in *GetApiInfoRequest, opts ...grpc.CallOption) (*GetApiInfoResponse, error)
+	// Create a new link session. This will return a URL to redirect the user to where they will be able to select a
+	// financial institution to connect to and log in.
+	CreateLinkSession(ctx context.Context, in *CreateLinkSessionRequest, opts ...grpc.CallOption) (*CreateLinkSessionResponse, error)
+	// Retrieve the status of a link session.
+	GetLinkSession(ctx context.Context, in *GetLinkSessionRequest, opts ...grpc.CallOption) (*GetLinkSessionResponse, error)
 }
 
 type firaServiceClient struct {
@@ -46,11 +53,34 @@ func (c *firaServiceClient) GetApiInfo(ctx context.Context, in *GetApiInfoReques
 	return out, nil
 }
 
+func (c *firaServiceClient) CreateLinkSession(ctx context.Context, in *CreateLinkSessionRequest, opts ...grpc.CallOption) (*CreateLinkSessionResponse, error) {
+	out := new(CreateLinkSessionResponse)
+	err := c.cc.Invoke(ctx, FiraService_CreateLinkSession_FullMethodName, in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *firaServiceClient) GetLinkSession(ctx context.Context, in *GetLinkSessionRequest, opts ...grpc.CallOption) (*GetLinkSessionResponse, error) {
+	out := new(GetLinkSessionResponse)
+	err := c.cc.Invoke(ctx, FiraService_GetLinkSession_FullMethodName, in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // FiraServiceServer is the server API for FiraService service.
 // All implementations must embed UnimplementedFiraServiceServer
 // for forward compatibility
 type FiraServiceServer interface {
 	GetApiInfo(context.Context, *GetApiInfoRequest) (*GetApiInfoResponse, error)
+	// Create a new link session. This will return a URL to redirect the user to where they will be able to select a
+	// financial institution to connect to and log in.
+	CreateLinkSession(context.Context, *CreateLinkSessionRequest) (*CreateLinkSessionResponse, error)
+	// Retrieve the status of a link session.
+	GetLinkSession(context.Context, *GetLinkSessionRequest) (*GetLinkSessionResponse, error)
 	mustEmbedUnimplementedFiraServiceServer()
 }
 
@@ -60,6 +90,12 @@ type UnimplementedFiraServiceServer struct {
 
 func (UnimplementedFiraServiceServer) GetApiInfo(context.Context, *GetApiInfoRequest) (*GetApiInfoResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method GetApiInfo not implemented")
+}
+func (UnimplementedFiraServiceServer) CreateLinkSession(context.Context, *CreateLinkSessionRequest) (*CreateLinkSessionResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method CreateLinkSession not implemented")
+}
+func (UnimplementedFiraServiceServer) GetLinkSession(context.Context, *GetLinkSessionRequest) (*GetLinkSessionResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetLinkSession not implemented")
 }
 func (UnimplementedFiraServiceServer) mustEmbedUnimplementedFiraServiceServer() {}
 
@@ -92,6 +128,42 @@ func _FiraService_GetApiInfo_Handler(srv interface{}, ctx context.Context, dec f
 	return interceptor(ctx, in, info, handler)
 }
 
+func _FiraService_CreateLinkSession_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(CreateLinkSessionRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(FiraServiceServer).CreateLinkSession(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: FiraService_CreateLinkSession_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(FiraServiceServer).CreateLinkSession(ctx, req.(*CreateLinkSessionRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _FiraService_GetLinkSession_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(GetLinkSessionRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(FiraServiceServer).GetLinkSession(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: FiraService_GetLinkSession_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(FiraServiceServer).GetLinkSession(ctx, req.(*GetLinkSessionRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // FiraService_ServiceDesc is the grpc.ServiceDesc for FiraService service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -102,6 +174,14 @@ var FiraService_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "GetApiInfo",
 			Handler:    _FiraService_GetApiInfo_Handler,
+		},
+		{
+			MethodName: "CreateLinkSession",
+			Handler:    _FiraService_CreateLinkSession_Handler,
+		},
+		{
+			MethodName: "GetLinkSession",
+			Handler:    _FiraService_GetLinkSession_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
