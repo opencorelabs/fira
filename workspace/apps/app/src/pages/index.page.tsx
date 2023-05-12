8c2e58db186bfc9cdc95bcb4be620155fafe59c4
harry.hexhash <129999410+harryhexhash@users.noreply.github.com>
2023-05-11 23:32:21 -0600

    package clean up

--- workspace/apps/app/src/pages/index.page.tsx
+++ workspace/apps/app/src/pages/index.page.tsx
@@ -1,4 +1,19 @@
+import { Api } from '@fira/api';
+import { useEffect } from 'react';
+
 export default function Index() {
+  useEffect(() => {
+    (async () => {
+      try {
+        const { api } = new Api({ baseUrl: 'http://localhost:8080' });
+        const response = await api.firaServiceGetApiInfo();
+        console.info('response', response);
+      } catch (error) {
+        console.error('error', error);
+      }
+    })();
+  }, []);
+
   return (
     <div>
       <h1>Dashboard</h1>
