1477bd1f145583fc4badf7c2219bd08d6a581632
pnwx <132782902+pnwx@users.noreply.github.com>
2023-05-13 20:57:16 -0700

    login method (#25)
    * login method
    
    * more tests

--- internal/api/service_accounts_test.go
+++ internal/api/service_accounts_test.go
@@ -3,6 +3,8 @@ package api
 import (
 	"context"
 	v1 "github.com/opencorelabs/fira/gen/protos/go/protos/fira/v1"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 func (s *FiraApiSuite) Test_CreateAccount_EmailPassword() {
@@ -25,6 +27,64 @@ func (s *FiraApiSuite) Test_VerifyAccount_EmailPassword() {
 	s.Require().NotNil(resp, "response is nil")
 
 	s.Equal(v1.AccountRegistrationStatus_ACCOUNT_REGISTRATION_STATUS_OK, resp.Status, "status is not ok")
+}
+
+func (s *FiraApiSuite) Test_LoginAccount_EmailPassword() {
+	s.verifiedAccount()
+
+	resp, loginErr := s.loginAccount()
+
+	s.Require().NoErrorf(loginErr, "failed to login account")
+	s.Require().NotNil(resp, "response is nil")
+	s.Require().Equal(v1.AccountRegistrationStatus_ACCOUNT_REGISTRATION_STATUS_OK, resp.Status, "status is not ok")
+	s.Require().NotEmpty(resp.Jwt, "jwt is empty")
+}
+
+func (s *FiraApiSuite) Test_LoginAccount_BeforeVerification_EmailPassword() {
+	_, createErr := s.createAccount()
+	s.Require().NoErrorf(createErr, "failed to create account")
+
+	resp, loginErr := s.loginAccount()
+
+	s.Require().NoErrorf(loginErr, "failed to login account")
+	s.Require().NotNil(resp, "response is nil")
+	// status should tell user to verify email
+	s.Require().Equal(v1.AccountRegistrationStatus_ACCOUNT_REGISTRATION_STATUS_VERIFY_EMAIL, resp.Status, "status is not verify email")
+	s.Require().NotEmpty(resp.Jwt, "jwt is not empty")
+}
+
+func (s *FiraApiSuite) Test_LoginAccount_NoAccountExists() {
+	req := &v1.LoginAccountRequest{
+		Credential: &v1.LoginCredential{
+			CredentialType: v1.AccountCredentialType_ACCOUNT_CREDENTIAL_TYPE_EMAIL,
+			EmailCredential: &v1.CredentialTypeEmail{
+				Email:    "idunno@opencorelabs.org",
+				Password: "simplepassword",
+			},
+		},
+	}
+
+	_, respErr := s.api.LoginAccount(context.Background(), req)
+	stat := status.Convert(respErr)
+	s.Equal(codes.Unauthenticated, stat.Code(), "status code is not unauthenticated")
+}
+
+func (s *FiraApiSuite) Test_LoginAccount_WrongPassword() {
+	s.verifiedAccount()
+	
+	req := &v1.LoginAccountRequest{
+		Credential: &v1.LoginCredential{
+			CredentialType: v1.AccountCredentialType_ACCOUNT_CREDENTIAL_TYPE_EMAIL,
+			EmailCredential: &v1.CredentialTypeEmail{
+				Email:    "pnwx@opencorelabs.org",
+				Password: "simplepassword-wrong",
+			},
+		},
+	}
+
+	_, respErr := s.api.LoginAccount(context.Background(), req)
+	stat := status.Convert(respErr)
+	s.Equal(codes.Unauthenticated, stat.Code(), "status code is not unauthenticated")
 }
 
 func (s *FiraApiSuite) createAccount() (*v1.CreateAccountResponse, error) {
@@ -58,4 +118,25 @@ func (s *FiraApiSuite) verifyAccount(email string) (*v1.VerifyAccountResponse, e
 	}
 
 	return s.api.VerifyAccount(context.Background(), req)
+}
+
+func (s *FiraApiSuite) loginAccount() (*v1.LoginAccountResponse, error) {
+	loginReq := &v1.LoginAccountRequest{
+		Credential: &v1.LoginCredential{
+			CredentialType: v1.AccountCredentialType_ACCOUNT_CREDENTIAL_TYPE_EMAIL,
+			EmailCredential: &v1.CredentialTypeEmail{
+				Email:    "pnwx@opencorelabs.org",
+				Password: "simplepassword",
+			},
+		},
+	}
+
+	return s.api.LoginAccount(context.Background(), loginReq)
+}
+
+func (s *FiraApiSuite) verifiedAccount() {
+	_, createErr := s.createAccount()
+	s.Require().NoErrorf(createErr, "failed to create account")
+	_, verifErr := s.verifyAccount("pnwx@opencorelabs.org")
+	s.Require().NoErrorf(verifErr, "failed to verify account")
 }
