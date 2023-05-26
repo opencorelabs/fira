695a89d038a3b2253c7d328dc49a66d85bddf339
harry.hexhash <129999410+harryhexhash@users.noreply.github.com>
2023-05-26 00:59:50 -0600

    add basepath

--- workspace/apps/fira-app/next.config.js
+++ workspace/apps/fira-app/next.config.js
@@ -2,6 +2,11 @@
 const config = {
   reactStrictMode: true,
   pageExtensions: ['page.tsx', 'api.ts'],
+  basePath: '/app',
+  images: {
+    domains: ['fira.opencorelabs.com'],
+    path: '/_next/image',
+  },
 };
 
 module.exports = config;
