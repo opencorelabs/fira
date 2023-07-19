68fe92d35f63cd6564783d11287467935cc13a65
pnwx <pnwx@opencorelabs.org>
2023-07-18 20:23:16 -0700

    api impls

--- backend/accounts/api.py
+++ backend/accounts/api.py
@@ -8,6 +8,21 @@ class EmailPasswordRegistration(Schema):
     full_name: str
     email_address: str
     password: str
+    verification_url: str
+
+
+class EmailVerification(Schema):
+    resend_to_email: str
+    verification_code: str
+
+
+class EmailPasswordLogin(Schema):
+    email_address: str
+    password: str
+
+
+class Token(Schema):
+    jwt: str
 
 
 class Account(Schema):
@@ -34,6 +49,49 @@ def create_account(request, data: EmailPasswordRegistration):
         full_name=data.full_name
     )
     return 201, Account(
+        email_address=user.email,
+        full_name=user.full_name,
+        verified=user.verified
+    )
+
+
+@router.post('/verify', response={200: Account, 400: InputError})
+def verify_account(request, data: EmailVerification):
+    user = User.objects.filter(verification_code=data.verification_code).first()
+    if user is None:
+        return 400, InputError(
+            fields={'verification_code': ['Invalid verification code']},
+            message='Invalid verification code'
+        )
+    user.verified = True
+    user.save()
+    return 200, Account(
+        email_address=user.email,
+        full_name=user.full_name,
+        verified=user.verified
+    )
+
+
+@router.post('/login', response={200: Token, 400: InputError})
+def login(request, data: EmailPasswordLogin):
+    user = User.objects.filter(email=data.email_address).first()
+    if user is None:
+        return 400, InputError(
+            fields={'email_address': ['Email address not found']},
+            message='Email address not found'
+        )
+    if not user.check_password(data.password):
+        return 400, InputError(
+            fields={'password': ['Incorrect password']},
+            message='Incorrect password'
+        )
+    return 200, Token(jwt=user.get_jwt())
+
+
+@router.get('/me', response={200: Account})
+def get_me(request):
+    user = request.auth
+    return 200, Account(
         email_address=user.email,
         full_name=user.full_name,
         verified=user.verified
